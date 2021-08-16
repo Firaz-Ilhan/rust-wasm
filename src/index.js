3e08849fa8a4725fa15677995e442d5ab63ddf1c
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-08-16 22:33:39 +0200

    add a few more examples

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ wasm.then(wasm => {
         <Header title="Rust webassembly test" />
         <div className="card_wrapper">
           <Test func={wasm.fib} title="Fibonacci sequence" />
+          <Test func={wasm.prime_factorization} title="Prime factorization" />
+          <Test func={wasm.is_armstrong_number} title="Is armstrong number?" />
+          <Test func={wasm.nth} title="nth prime" />
         </div>
       </div>
     );
--- src/lib.rs
+++ src/lib.rs
@@ -27,5 +27,54 @@ pub fn fib(n: i64) -> u64 {
         last = current;
         current = sum;
     }
-    sum
+    return sum;
+}
+
+#[wasm_bindgen]
+pub fn prime_factorization(mut n: u64) -> Vec<u64> {
+    let mut factors = Vec::new();
+    let mut candidates = 2..;
+
+    while n > 1 {
+        let x = candidates.next().unwrap();
+
+        while n % x == 0 {
+            n /= x;
+            factors.push(x);
+        }
+    }
+
+    return factors;
+}
+
+#[wasm_bindgen]
+pub fn is_armstrong_number(n: u32) -> bool {
+    let n_str = n.to_string();
+    let num_len = n_str.len() as u32;
+
+    let sum = n_str
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .map(|d| d.pow(num_len))
+        .sum();
+
+    n == sum
+}
+
+#[wasm_bindgen]
+pub fn is_prime(n: u32) -> bool {
+    for a in 2..n {
+        if n % a == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+#[wasm_bindgen]
+pub fn nth(n: u32) -> Option<u32> {
+    match n {
+        n if n <= 0 => None,
+        n => (1..).filter(|c| is_prime(*c)).nth(n as usize),
+    }
 }
