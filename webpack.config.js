fca0bd2de79470cd4a1cd4d04c432237189e3b1b
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-09-14 20:57:00 +0200

    code cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
         path: path.resolve(__dirname, "public/dist")
     },
     devServer: {
-        contentBase: path.join(__dirname, 'public'),
         compress: true,
     },
 
